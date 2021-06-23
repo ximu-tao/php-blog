fbeea3df5b0c6d64a2909db7625a9c30758eb878
ximu-tao <me_litao@outlook.com>
2021-06-23 17:27:04 +0800

    更新

---
+++ js/apiurl.js
@@ -0,0 +1,11 @@
+apiUrlList = {
+    "checkserver" : "./api/check/checkserver.php",
+    "hasname" : "./api/check/hasname.php",
+    "unlog" : "./api/unlog.php",
+    "getuserdata" : "./api/get/getuserdata.php",
+    "getbloglist" : "./api/get/getbloglist.php",
+    "login" : "./api/login.php",
+    "uploadAvatar" : "./api/upload/avatar.php",
+    "signup" : "./api/signup.php",
+    "publishblog" : "./api/publish/publishblog.php"
+}
